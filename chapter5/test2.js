19272f37300cc91a30cfb07bb7b324762a91875f
Tejas Mehta <tmehta@apple.com>
2016-05-26 15:26:17 -0700

    more

--- chapter5/test2.js
+++ chapter5/test2.js
@@ -53,7 +53,21 @@ var ancestry = JSON.parse(ANCESTRY_FILE);
 
 // console.log(min);
 
+//composability
 
+function average(array) {
+	function plus(a, b) {
+		return a + b;
+	}
+	return array.reduce(plus) / array.length;
+}
+
+function age(p) { return p.died - p.born; }
+function male(p) { return p.sex == "m"; }
+function female(p) { return p.sex == "f"; }
+
+console.log(average(ancestry.filter(male).map(age)));
+console.log(average(ancestry.filter(female).map(age)));
 
 
 // function filter(array, test){
